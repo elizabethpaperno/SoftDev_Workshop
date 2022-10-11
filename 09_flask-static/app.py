963d902378d0f3113636e9085cfa76fdb2a160e3
Elizabeth Paperno <liza.paperno@gmail.com>
2022-10-11 09:23:40 -0400

    add header

--- 09_flask-static/app.py
+++ 09_flask-static/app.py
@@ -1,9 +1,23 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Oct 2022
+'''
+TEAM KrEWes: Elizabeth Paperno, Raven (Ruiwen) Tang, William Vongphanith, Kosta Dubovskiy
+SoftDev
+K09 -- Using Flask to Display Static Files
+2022-10-11
+Time Spent: 0.6 hours
 
-# DEMO
-# basics of /static folder
+DISCO
+___________
+- If the file has a html extension the server will be created and display on the webpage how the html normally would.
+- If a file does not have an extension its contents will either be displayed or the file will attempt to be downloaded. This behavior varies based on the browser used.
+- You can use localhost as opposed to an IP address.
+- The file path follows the same path as the directory where app is routed.
+
+QCC
+___________
+- Can we change if the file is downloaded or displayed with server configuration?
+- What other file extenstions can be displayed properly (in addition to html)?
+
+'''
 
 from flask import Flask
 app = Flask(__name__)
