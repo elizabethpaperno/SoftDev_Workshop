e20e3b890458113f177907015410bec5ea2ba8ff
Elizabeth Paperno <liza.paperno@gmail.com>
2022-10-17 09:26:41 -0400

    class time

--- 12_flask-forms/app.py
+++ 12_flask-forms/app.py
@@ -3,14 +3,18 @@ from flask import Flask, render_template, request #import modules
 
 app = Flask(__name__)    #create Flask object
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def disp_loginpage():
     return render_template( 'login.html' )
 
-@app.route("/auth") # /auth is referenced in form action in the html #, methods=['GET', 'POST']) # By default the user can both post and request info but passing the methods parameter specifies which operations can be done
+@app.route("/auth", methods=['GET', 'POST']) # /auth is referenced in form action in the html #, methods=['GET', 'POST']) # By default the user can both post and request info but passing the methods parameter specifies which operations can be done
 def authenticate():
+    if request.method =='POST':
+        print("asfd")
+        return render_template("response.html", name=request.form['name'], req=str(request))
     name = request.args['name']
-    return render_template('response.html', name=name)
+    req = str(request)
+    return render_template('response.html', name=name, req=req)
 
 
 if __name__ == "__main__":  # true if this file NOT imported
