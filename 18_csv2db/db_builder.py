39b51ca98545a8e41d2b425da6db44505211fc05
Elizabeth Paperno <liza.paperno@gmail.com>
2022-10-26 09:40:53 -0400

    add new changes

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -15,6 +15,7 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 #==========================================================
 
 def popTable(filename):
+    '''
     with open(filename) as f:
         csvfile = csv.reader(f)
         titles = next(csvfile)
@@ -25,6 +26,31 @@ def popTable(filename):
 
         for row in csvfile:
             c.execute(f"INSERT INTO {tblname} VALUES (\"{row[0]}\", {row[1]}, {row[2]})")
+    '''
+
+    #qwrking but seeems inefficent
+    with open("students.csv") as f:
+        csvfile = csv.DictReader(f)
+        #print(csvfile)
+        tblname = "students"
+
+        c.execute(f"DROP TABLE IF EXISTS {tblname}")
+        c.execute(f"CREATE TABLE {tblname}(name TEXT, age INTEGER, id INTEGER)")
+
+        for row in csvfile:
+            c.execute(f"INSERT INTO {tblname} VALUES (\"{row['name']}\", {row['age']}, {row['id']})")
+    with open("courses.csv") as f:
+        csvfile = csv.DictReader(f)
+        #print(csvfile)
+        tblname = "courses"
+
+        c.execute(f"DROP TABLE IF EXISTS {tblname}")
+        c.execute(f"CREATE TABLE {tblname}(code TEXT, mark INTEGER, id INTEGER)")
+
+        for row in csvfile:
+            c.execute(f"INSERT INTO {tblname} VALUES (\"{row['code']}\", {row['mark']}, {row['id']})")
+
+
 
 
 if __name__ == "__main__":
@@ -34,11 +60,9 @@ if __name__ == "__main__":
         popTable(f)
         tablename=f.split(".")[0]
         c.execute(f"SELECT * FROM {tablename}")
-    
+
 
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
-
