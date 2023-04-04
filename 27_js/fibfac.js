1a4726c8fec91bde528e0d6bc361d62475dc9d27
Elizabeth Paperno <liza.paperno@gmail.com>
2023-04-04 13:18:07 -0400

    js work

---
+++ 27_js/fibfac.js
@@ -0,0 +1,31 @@
+// Team TeePee :: Jeffery Tang, Elizabeth Paperno
+// SoftDev pd7
+// K27 -- Basic functions in JavaScript
+// 2023-04-04t
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+
+function fac(n){
+    if (n < 2) {
+        return 1;
+    }
+    else {
+        return n * fib(n-1);
+    }
+}
+
+function fib(n){
+    if(n == 0){
+        return 0;
+    } else if (n == 1){
+        return 1;
+    } else{
+        return fib(n-1) + fib(n-2);
+    }
+}
