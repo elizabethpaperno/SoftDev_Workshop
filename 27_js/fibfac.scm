02a7e25cd2fd4f0ba99d97f1681ed319473a3581
Elizabeth Paperno <liza.paperno@gmail.com>
2023-04-04 20:35:09 -0400

    fixed syntax

--- 27_js/fibfac.scm
+++ 27_js/fibfac.scm
@@ -9,31 +9,49 @@
 ;<your team's fact(n) implementation>
 
 
-(define fact (lambda(n))
-    (if(< n 2))
+(define fact 
+  (lambda(n)
+    (if(< n 2)
         1
-    (*n(fact (- n 1)))
+    (* n (fact (- n 1)))
+    )
+  )
 )
 
-fact(1) ;"...should be  1"
-fact(2) ;"...should be  2"
-fact(3) ;"...should be  6"
-fact(4) ;"...should be  24"
-fact(5) ;"...should be  120"
+(fact 1)
+(fact 2)
+(fact 3)
+(fact 4)
+(fact 5)
+;fact(1) ;"...should be  1"
+;fact(2) ;"...should be  2"
+;fact(3) ;"...should be  6"
+;fact(4) ;"...should be  24"
+;fact(5) ;"...should be  120"
 
 
 ;fib:
 ;<your team's fib(n) implementation>
-(define fib(lambda(n))
-    (if(= n 0))
-        0
-    (if(= n 1))
+(define fib
+  (lambda(n)
+     (if(= n 1)
         1
-    (+ (fib(n-1)) (fib(n-2)))
+        (if (= n 0)
+         0
+         (+ (fib(- n 1)) (fib(- n 2)))
+        )
+     )
+   )
 )
-fib(0) ;"...should be  0"
-fib(1) ;"...should be  1"
-fib(2) ;"...should be  1"
-fib(3) ;"...should be  2"
-fib(4) ;"...should be  3"
-=================================================================
+
+ 
+(fib 0)
+(fib 1)
+(fib 2)
+(fib 3)
+(fib 4)
+;fib(0) ;"...should be  0"
+;fib(1) ;"...should be  1"
+;fib(2) ;"...should be  1"
+;fib(3) ;"...should be  2"
+;fib(4) ;"...should be  3"
